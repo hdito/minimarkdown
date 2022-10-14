5c7ed35a7734ec77a4c3b4021970204afb989833
hdito <hditow@gmail.com>
2022-10-14 19:40:33 +0300

    Change size of the button to prevent accidental taps

--- src/Texts.tsx
+++ src/Texts.tsx
@@ -15,7 +15,7 @@ export const Texts = () => {
     <>
       <button
         onClick={() => dispatch(addText(uid as string))}
-        className="relative flex gap-2 sm:gap-1 sm:flex-col justify-center items-center p-2 sm:h-[240px] h-fit border-2 border-black dark:border-gray-100 rounded-md shadow-md hover:shadow-lg dark:shadow-none dark:hover:shadow-none"
+        className="relative flex gap-2 sm:gap-1 sm:flex-col self-start justify-center items-center p-2 sm:h-[240px] h-fit border-2 border-black dark:border-gray-100 rounded-md shadow-md hover:shadow-lg dark:shadow-none dark:hover:shadow-none"
       >
         <HiOutlinePlus className="text-xl sm:text-4xl" />
         {t('createNewText')}
