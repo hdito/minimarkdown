7c319aac72f92d2e6c623a4f6641ca92c6bd43ba
hdito <hditow@gmail.com>
2022-11-10 00:11:58 +0300

    Add handling of incorrect urls

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { rootState } from './app/main';
 import { EditMode } from './EditMode';
 import { LoadingSpinner } from './LoadingSpinner';
@@ -18,25 +18,46 @@ export const Editor = () => {
   const [draft, setDraft] = useState(text?.content ?? '');
   const [isEditMode, setIsEditMode] = useState(state?.isEditMode ?? true);
   const { ready } = useTranslation('translation', { useSuspense: false });
+  const { t } = useTranslation();
   return (
     <div className="text-black dark:text-gray-50 min-h-screen flex flex-col bg-white dark:bg-gray-800">
-      {!isLoading && ready ? (
+      {text ? (
         <>
-          {isEditMode ? (
-            <EditMode
-              draft={draft}
-              onChange={(e) => setDraft(e.currentTarget.value)}
-              onShowPreview={() => setIsEditMode(false)}
-            />
+          {!isLoading && ready ? (
+            <>
+              {isEditMode ? (
+                <EditMode
+                  draft={draft}
+                  onChange={(e) => setDraft(e.currentTarget.value)}
+                  onShowPreview={() => setIsEditMode(false)}
+                />
+              ) : (
+                <Preview
+                  draft={draft}
+                  onShowEditor={() => setIsEditMode(true)}
+                />
+              )}
+              <Menu draft={draft} />
+              <SaveMessage />
+            </>
           ) : (
-            <Preview draft={draft} onShowEditor={() => setIsEditMode(true)} />
+            <div className="flex-1 flex items-center justify-center">
+              <LoadingSpinner />
+            </div>
           )}
-          <Menu draft={draft} />
-          <SaveMessage />
         </>
       ) : (
-        <div className="flex-1 flex items-center justify-center">
-          <LoadingSpinner />
+        <div className="py-2 px-4">
+          <h1 className="text-2xl sm:text-4xl font-bold mb-4">
+            {t('404.header')}
+          </h1>
+          <p>
+            {t('404.description')}{' '}
+            <Link className="underline" to="/">
+              {t('404.mainMenu')}
+            </Link>
+            .
+          </p>
         </div>
       )}
     </div>
